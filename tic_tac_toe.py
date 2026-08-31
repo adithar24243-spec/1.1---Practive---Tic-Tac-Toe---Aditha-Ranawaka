1a883041c18a4071171648e5a4348f46e3529ae8
Aditha Ranawaka <adithar.24243@obhs.school.nz>
2026-08-31 16:36:47 +1200

    Display the board
    Display the board with correct spacing and names

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -19,6 +19,24 @@ def create_board():
             board.append = []
     return board 
 
+def display_board(board):
+    row_numbers = " "
+    for j in range(3):
+        row_numbers = row_numbers + " " + str(j+1) + ""
+
+    print("        " + row_numbers)
+
+    row_string = ""
+
+    for j in range(3):
+        row_string = row_string + "[" + board[i][j] + "]"
+
+    if(i == 1):
+        print("[column]" + str(i) " " + (row_string))
+    else:
+        print("        " + str(i) + " " + (row_string))
+
+
 #--------------------------main routine------------------------
 
 create_board()
