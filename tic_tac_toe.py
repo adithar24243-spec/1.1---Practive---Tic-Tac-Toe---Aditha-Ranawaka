973c03ece78f28c9887aa95e60710b4d1c19626b
Aditha Ranawaka <adithar.24243@obhs.school.nz>
2026-08-31 15:59:35 +1200

    Start
    Writing the structure

---
+++ tic_tac_toe.py
@@ -0,0 +1,14 @@
+'''
+    author: Aditha Mansilu Ranawaka
+    date: 26/8/2026
+    version: 
+    description: Rock, paper, scissor game
+''' 
+
+#--------------------------libraries---------------------------
+
+
+#--------------------------functions---------------------------
+
+#--------------------------main routine------------------------
+
